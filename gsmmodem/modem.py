6cdc5e6fcce262f45e3aadf2424d3fad9309758a
Andrii Paramonov <Paramon@users.noreply.github.com>
2017-11-30 13:42:40 +0200

    Update modem.py
    #HTTPCALL - Telik modem

--- gsmmodem/modem.py
+++ gsmmodem/modem.py
@@ -146,12 +146,13 @@ class GsmModem(SerialComms):
     CDSI_REGEX = re.compile('\+CDSI:\s*"([^"]+)",(\d+)$')
     CDS_REGEX  = re.compile('\+CDS:\s*([0-9]+)"$')
 
-    def __init__(self, port, baudrate=115200, incomingCallCallbackFunc=None, smsReceivedCallbackFunc=None, smsStatusReportCallback=None, requestDelivery=True, AT_CNMI="", *a, **kw):
+    def __init__(self, port, baudrate=115200, incomingCallCallbackFunc=None, smsReceivedCallbackFunc=None, smsStatusReportCallback=None, requestDelivery=True, AT_CNMI="",httpCallbackFunc=None, *a, **kw):
         super(GsmModem, self).__init__(port, baudrate, notifyCallbackFunc=self._handleModemNotification, *a, **kw)
         self.incomingCallCallback = incomingCallCallbackFunc or self._placeholderCallback
         self.smsReceivedCallback = smsReceivedCallbackFunc or self._placeholderCallback
         self.smsStatusReportCallback = smsStatusReportCallback or self._placeholderCallback
         self.requestDelivery = requestDelivery
+        self.httpCallback = httpCallbackFunc
         self.AT_CNMI = AT_CNMI or "2,1,0,2"
         # Flag indicating whether caller ID for incoming call notification has been set up
         self._callingLineIdentification = False
@@ -1181,8 +1182,12 @@ class GsmModem(SerialComms):
         :param lines The lines that were read
         """
         next_line_is_te_statusreport = False
+
         for line in lines:
-            if 'RING' in line:
+            if line.startswith('#HTTPRING'):
+                self.httpCallback(line)
+                return
+            elif 'RING' in line:
                 # Incoming call (or existing call is ringing)
                 self._handleIncomingCall(lines)
                 return
